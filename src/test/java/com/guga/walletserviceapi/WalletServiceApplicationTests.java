cecd444171afdbe904e4bff3aa2136f8d9c2264d
Gustavo Souza <gustavo1282@gmail.com>
2025-10-24 18:28:08 -0300

    ciAtualizações Sonarqube local - correcao dos tests JUnit

--- src/test/java/com/guga/walletserviceapi/WalletServiceApplicationTests.java
+++ src/test/java/com/guga/walletserviceapi/WalletServiceApplicationTests.java
@@ -1,22 +1,32 @@
 package com.guga.walletserviceapi;
 
-import org.hibernate.cfg.Environment;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 class WalletServiceApplicationTests {
 
     @Value("${spring.profiles.active:default}")
     private String activeProfile;
 
+    @Value("${controller.path.base}")
+    private String BASE_PATH;
+
     @Test
 	void contextLoads() {
+        System.out.println("============================================");
+        System.out.println("WalletServiceApplicationTests > contextLoads()");
 
         System.out.println("spring.profiles.active = ".concat(activeProfile) );
+        System.out.println("BASE_PATH = ".concat(BASE_PATH) );
 
-	}
+        System.out.println("============================================");
+    }
 
 }
